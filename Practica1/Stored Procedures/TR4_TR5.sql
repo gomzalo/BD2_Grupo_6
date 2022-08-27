41484a296324e819511a32ff453f718f558452b3
LUIS FERNANDO MORALES GARCÍA <2984413810101@ingenieria.usac.edu.gt>
2022-08-27 10:12:30 -0600

    error almacenado en log para TR4 Y TR5

--- Practica1/Stored Procedures/TR4_TR5.sql
+++ Practica1/Stored Procedures/TR4_TR5.sql
@@ -6,17 +6,24 @@ use BD2;
 CREATE PROCEDURE TR4
 	@Nombre VARCHAR(100)
 AS
-BEGIN
-	IF (SELECT [RoleName] FROM practica1.Roles WHERE [RoleName] = @Nombre) = @Nombre
-		PRINT 'Ya existe el ROL'		
-	ELSE
-		--PRINT 'SE FUE AL ELSE'
-		INSERT INTO practica1.Roles ([Id],[RoleName]) VALUES (NEWID(),@Nombre);
-END;
+BEGIN TRY
+	BEGIN TRANSACTION;
+		IF (SELECT [RoleName] FROM practica1.Roles WHERE [RoleName] = @Nombre) = @Nombre
+			INSERT INTO practica1.HistoryLog([Date],[Description])
+			SELECT GETDATE(),CONCAT('Error en TR4, ya existe el ROL ',@Nombre)
+			--PRINT 'Ya existe el ROL'	
+		ELSE
+			
+			INSERT INTO practica1.Roles ([Id],[RoleName]) VALUES (NEWID(),@Nombre);
+	COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+	INSERT INTO practica1.HistoryLog([Date],[Description])
+	SELECT GETDATE(),CONCAT('Error en TR4, tabla ROLES ',ERROR_MESSAGE()) AS ErrorMessage;
+	ROLLBACK TRANSACTION;
+END CATCH
 GO
 EXEC TR4 'Tutor';
-GO
-select * from practica1.Roles;
 GO
 ------------------------------------------------------------
 ------------------------------------------------------------
@@ -28,14 +35,25 @@ CREATE PROCEDURE TR5
 	@Nombre VARCHAR(100),
 	@Credits INT
 AS
-BEGIN
-	IF (SELECT [Name] FROM practica1.Course WHERE [Name] = @Nombre) = @Nombre
-		PRINT 'Ya existe el CURSO'		
-	ELSE
-		--PRINT 'SE FUE AL ELSE'
-		INSERT INTO practica1.Course([CodCourse],[Name],[CreditsRequired]) VALUES (@code,@Nombre,@Credits);
-END;
+BEGIN TRY
+	BEGIN TRANSACTION;
+		IF (SELECT [Name] FROM practica1.Course WHERE [Name] = @Nombre) = @Nombre
+			INSERT INTO practica1.HistoryLog([Date],[Description])
+			SELECT GETDATE(),CONCAT('Error en TR5, ya existe el Curso ',@Nombre)
+			--PRINT 'Ya existe el CURSO'		
+		ELSE
+			--PRINT 'SE FUE AL ELSE'
+			INSERT INTO practica1.Course([CodCourse],[Name],[CreditsRequired]) VALUES (@code,@Nombre,@Credits);
+	COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+	INSERT INTO practica1.HistoryLog([Date],[Description])
+	SELECT GETDATE(),CONCAT('Error en TR5, tabla COURSE ',ERROR_MESSAGE()) AS ErrorMessage;
+	ROLLBACK TRANSACTION;
+END CATCH
 GO
 EXEC TR5 555,N'Curso de prueba',32;
 GO
 select * from practica1.Course;
+select * from practica1.Roles;
+select * from practica1.HistoryLog;
