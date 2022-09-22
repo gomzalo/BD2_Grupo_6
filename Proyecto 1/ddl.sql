335a610690ab6acd78d5b8fae452e826112df7a9
MacoChave <201020831@ingenieria.usac.edu.gt>
2022-09-21 18:49:06 -0600

    DDL y Backups

---
+++ Proyecto 1/ddl.sql
@@ -0,0 +1,85 @@
+SHOW DATABASES;
+
+-- --------------------------------------------------
+-- PRIMER DIA
+-- --------------------------------------------------
+-- CREAR DB
+DROP DATABASE DB_CMEDICO;
+CREATE DATABASE DB_CMEDICO
+CHARACTER SET utf8
+COLLATE utf8_general_ci;
+
+USE DB_CMEDICO;
+
+-- CREAR TABLAS
+
+CREATE TABLE PACIENTE (
+	idPaciente INT,
+    edad INT,
+    genero VARCHAR(20),
+    CONSTRAINT pk_paciente PRIMARY KEY(idPaciente)
+);
+
+CREATE TABLE HABITACION (
+	idHabitacion INT,
+    habitacion VARCHAR(50),
+    CONSTRAINT pk_habitacion PRIMARY KEY (idHabitacion)
+);
+
+CREATE TABLE LOG_HABITACION (
+	id_log_habitacion INT AUTO_INCREMENT,
+	timestampx VARCHAR(100),
+    statusx VARCHAR(45),
+    idHabitacion INT,
+    CONSTRAINT pk_loghabitacion PRIMARY KEY (id_log_habitacion),
+    CONSTRAINT fk_habitacionlog FOREIGN KEY (idHabitacion) REFERENCES HABITACION(idHabitacion)
+);
+
+CREATE TABLE LOG_ACTIVIDAD (
+	id_log_actividad INT AUTO_INCREMENT,
+    timestampx VARCHAR(100),
+    actividad VARCHAR(500),
+    idPaciente INT,
+    idHabitacion INT,
+    CONSTRAINT pk_logactividad PRIMARY KEY (id_log_actividad),
+    CONSTRAINT fk_pacienteactividad FOREIGN KEY (idPaciente) REFERENCES PACIENTE(idPaciente),
+    CONSTRAINT fk_habitacionactividad FOREIGN KEY (idHabitacion) REFERENCES HABITACION(idHabitacion)
+);
+
+-- CARGA DE DATOS
+LOAD DATA
+INFILE 'C:\\CSVInputFiles\\Habitaciones.csv'
+INTO TABLE HABITACION
+CHARACTER SET UTF8
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS;
+
+-- FULL BACKPU
+-- mysqldump -u root -p DB_CMEDICO > fullbackup_dia1.sql
+-- mysql -u root -p DB_CMEDICO < fullbackup_dia1.sql
+
+-- INCREMENTAL BACKUP
+-- EN MySQL Command Line:
+-- GENERAR BINARY LOGS
+-- flush logs;
+-- show
+-- EN Consola normal
+-- mysqlbinlog -v "C:\ProgramData\MySQL\MySQL Server 8.0\Data\DESKTOP-5JRB2P8-bin.000136"
+
+-- LIMIT
+SELECT * FROM PACIENTE LIMIT 20;
+SELECT * FROM HABITACION LIMIT 20;
+SELECT * FROM LOG_HABITACION LIMIT 20;
+SELECT * FROM LOG_ACTIVIDAD LIMIT 20;
+
+-- ORDER BY
+SELECT * FROM PACIENTE ORDER BY 1 DESC LIMIT 20;
+SELECT * FROM HABITACION ORDER BY 1 DESC LIMIT 20;
+SELECT * FROM LOG_HABITACION ORDER BY 1 DESC LIMIT 20;
+SELECT * FROM LOG_ACTIVIDAD ORDER BY 1 DESC LIMIT 20;
+
+-- COUNT
+SELECT COUNT(*) FROM PACIENTE;
+SELECT COUNT(*) FROM HABITACION;
+SELECT COUNT(*) FROM LOG_HABITACION;
+SELECT COUNT(*) FROM LOG_ACTIVIDAD;
