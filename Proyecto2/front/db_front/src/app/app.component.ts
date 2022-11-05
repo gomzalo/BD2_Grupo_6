c41c94e545bc4f97ee0cb130362e077d97fd0c04
LUIS FERNANDO MORALES GARCÍA <2984413810101@ingenieria.usac.edu.gt>
2022-11-05 11:47:16 -0600

    front terminado x2

--- Proyecto2/front/db_front/src/app/app.component.ts
+++ Proyecto2/front/db_front/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ConnectionService } from './connection.service';
+interface Food {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  consultaSelected:string;
+  dbSelected:string;
+  resultado:string;
+
+  constructor(public conection: ConnectionService) {}
+
   title = 'db_front';
+  foods: Food[] = [
+    {value: '1.1', viewValue: 'Total de pacientes que llegan a la clínica menores de 18 años'},
+    {value: '1.2', viewValue: 'Total de pacientes que llegan a la clínica entre 18 y 60 años'},
+    {value: '1.3', viewValue: 'Total de pacientes que llegan a la clínica mayores a 60 años'},
+    {value: '2', viewValue: 'Cantidad de pacientes que pasan por cada habitación'},
+    {value: '3', viewValue: 'Cantidad de pacientes que llegan a la clínica, agrupados por género'},
+    {value: '4', viewValue: 'Top 5 edades más atendidas en la clínica'},
+    {value: '5', viewValue: 'Top 5 edades menos atendidas en la clínica'},
+    {value: '6', viewValue: 'Top 5 habitaciones más utilizadas'},
+    {value: '7', viewValue: 'Top 5 habitaciones menos utilizadas'},
+    {value: '8', viewValue: 'Día con más pacientes en la clínica'},
+  ];
+
+
+  dbs:Food[] = [
+    {value: '1', viewValue: 'Redis'},
+    {value: '2', viewValue: 'MongoDB'},
+    {value: '3', viewValue: 'MySQL'},
+    {value: '4', viewValue: 'Cassandra'},
+  ]
+
+
+  consulta(){
+    const consulta = {db: this.dbSelected, consulta: this.consultaSelected};
+    console.log(consulta);
+    /*this.conection.getConsulta(consulta).subscribe((res: any) => {
+      console.log(res);
+      resultado = res;
+    });*/
+  }
 }
--- Proyecto2/front/db_front/src/app/app.module.ts
+++ Proyecto2/front/db_front/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSelectModule} from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
 
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
