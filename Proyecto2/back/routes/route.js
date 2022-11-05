241586e839155e3fde1cb20797a3958d790cdd04
A <201318652@ingenieria.usac.edu.gt>
2022-11-05 12:21:31 -0600

    siuuu

--- Proyecto2/back/routes/route.js
+++ Proyecto2/back/routes/route.js
@@ -8,9 +8,9 @@ const LogActividades = require('../models/LogActividad');
 
 router.get('/', controller.root);
 router.get('/1a', controller.get_1a);
-// router.get('/1b', controller.get_1b);
-// router.get('/1c', controller.get_1c);
-// router.get('/2', controller.get_2);
+router.get('/1b', controller.get_1b);
+router.get('/1c', controller.get_1c);
+router.get('/2', controller.get_2);
 // router.get('/3', controller.get_3);
 // router.get('/4', controller.get_4);
 // router.get('/5', controller.get_5);
